2c39692dbcd71177cd9f43af4e3befc88d987a5a
Vikash <vikash@programminggeek.in>
2018-03-02 20:06:35 +0530

    Add job & home component

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { JobComponent } from './job/job.component';
+
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    // outlet: 'popup'
+  },
+  {
+    path: 'job',
+    component: JobComponent
+  },  // { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {
+        enableTracing: true, // <-- debugging purposes only
+        // preloadingStrategy: SelectivePreloadingStrategy,
+
+      }
+    )
+  ],
+  exports: [
+    RouterModule
+  ],
+  providers: [
+    // CanDeactivateGuard,
+    // SelectivePreloadingStrategy
+  ]
+})
+export class AppRoutingModule {
+}
